badadf7f88a0f6da191f77685f159e660a922ae0
austino1 <32141431+austino1@users.noreply.github.com>
2017-10-08 17:07:32 -0700

    cleaned up some of the formatting

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-	
-	console.log("Im on!")
 
-// how to move the player
-	var gas = function(keypressEvent){
-		var keyCode = String.fromCharCode(keypressEvent.keyCode);
-		var $lane = $('[data-key="' + keyCode + '"]');
-    	var leftPosition = $lane.offset().left;
-    	$lane.css({left: leftPosition + 6});
-	}
+  console.log("Im on!")
 
-	var race = function() {
+  // how to move the player
+  var gas = function(keypressEvent) {
+    var keyCode = String.fromCharCode(keypressEvent.keyCode);
+    var $lane = $('[data-key="' + keyCode + '"]');
+    var leftPosition = $lane.offset().left;
+    $lane.css({
+      left: leftPosition + 10
+    });
+  }
+
+  var race = function() {
     $(window).on('keypress', function(event) {
-        gas(event);
-     });
+      gas(event);
+    });
   };
   race();
 
